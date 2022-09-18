0a12d29843ac60330f978708ae8ad0eb3c35b04d
liuyi <>
2022-09-18 19:37:19 +0800

    feat: use koa logger

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const Koa = require('koa')
 const cors = require('koa2-cors')
 const koaBody = require('koa-body')
+const logger = require('koa-logger')
 const router = require('./router')
 const envJson = require('./.env.json')
 const { initDB } = require('./utils/mongodb')
@@ -9,6 +10,7 @@ const { initDB } = require('./utils/mongodb')
 // 实例化
 const app = new Koa()
 app.use(cors())
+app.use(logger())
 
 // 支持文件上传
 app.use(koaBody({
@@ -22,7 +24,10 @@ app.use(koaBody({
 // 启动路由
 router(app)
 
-// const env = process.env.NODE_ENV || 'development'
+// app错误监听
+app.on('error', (err) => {
+  console.error('Server error: \n%s\n%s ', err.stack || '')
+})
 
 app.listen(envJson.appPort, async () => {
     console.log(`app runs on port ${ envJson.appPort }.`)
